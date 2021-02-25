45e9b3dea5e032a76ddd29bcb1167d78dafa525e
Brandon Counts <bran.counts1@gmail.com>
2021-02-25 02:57:59 -0500

    cleanup

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,6 +1,5 @@
 import config from "../config.js";
 import jsonwebtoken from "jsonwebtoken";
-import logger from "../utilities/logger.js";
 import userModel from "../models/userModel.js";
 
 const {
@@ -16,22 +15,19 @@ export const createToken = (user) => {
 export const httpAuthMiddleware = async (req, res, next) => {
   try {
     const cookie = req.cookies.ct_session;
-    console.log(cookie, '----------------------------')
-    if (req.originalUrl !== "/api/users/login" && cookie) {
-      const foundUser = await userModel.findById(cookie);
-      console.log(foundUser, '----------------------------')
-      req.user = {
-        id: foundUser._id,
-        username: foundUser.username,
-        photoURL: foundUser.photoURL,
-        status: foundUser.status,
-      };
+    if (req.originalUrl !== "/api/users/login") {
+      if (cookie) {
+        const foundUser = await userModel.findById(cookie);
+        req.user = {
+          id: foundUser._id,
+          username: foundUser.username,
+          photoURL: foundUser.photoURL,
+          status: foundUser.status,
+        };
+        next();
+      } else res.status(401).send("you aint gotta cookie");
     }
-    next();
   } catch (error) {
     res.status(401).send("you aint gotta cookie");
-
-    next();
-    // normally send a 401
   }
 };
